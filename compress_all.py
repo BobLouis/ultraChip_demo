14a472d1709f96a9b7b948b6ff6c0907add4c280
BobLouis <bob900316@gmail.com>
2023-08-31 21:30:47 +0800

    fix

--- compress_all.py
+++ compress_all.py
@@ -3,13 +3,15 @@ import subprocess
 
 # Get all files that start with "BLOCK"
 files = [f for f in os.listdir() if f.startswith('BLOCK')]
-# subprocess.run(["gcc", "lzss.c", "-o", "lzss"])
-# subprocess.run(["gcc", "lzss_split.c", "-o", "lzss_split"])
+subprocess.run(["gcc", "lzss.c", "-o", "lzss"])
+subprocess.run(["gcc", "lzss_split.c", "-o", "lzss_split"])
 
 sum = 0
 cnt = 0
+block_size_sum = 0
 for file in files:
     # Echo the filename
+    print("=======================================================")
     print(file)
 
     # Execute the trans.py with --split argument
@@ -22,6 +24,7 @@ for file in files:
     subprocess.run(["./lzss", "e", "output.txt", "compress.txt"])
     all_total_output = subprocess.run(["./lzss", "e", "output.txt", "compress.txt"], capture_output=True, text=True).stdout
     all_total = int(all_total_output.split('total:')[1].split()[0])
+    block_size_sum += int(all_total_output.split('text:')[1].split()[0])
 
     # split
     print("split")
@@ -32,5 +35,8 @@ for file in files:
     sum += min(all_total, split_total)
     cnt += 1
 
+print("=======================================================")
 print("all sum ", sum, "bytes")
 print("average ", sum/cnt, "bytes")
+print("compress ratio(high): ",sum*100/(cnt*643),"%" )
+print("compress ratio(low): ",sum*100/(block_size_sum),"%" )
